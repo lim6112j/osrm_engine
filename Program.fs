e495f66ae3bd15459c8f10be768796d701924a6c
lim6112j <lim6112j@gmail.com>
2023-08-19 10:46:02 +0900

    suave crud api made

--- Program.fs
+++ Program.fs
@@ -1,5 +1,7 @@
 namespace OSRMEngine
+
 #nowarn "20"
+
 open System
 open System.Collections.Generic
 open System.IO
@@ -14,23 +16,42 @@ open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 
+
 module Program =
     let exitCode = 0
+
+    open Suave.Web
+
+    open OSRMEngine.Services
+    open OSRMEngine.Repository
+
 
     [<EntryPoint>]
     let main args =
 
-        let builder = WebApplication.CreateBuilder(args)
+        // let builder = WebApplication.CreateBuilder(args)
+        //
+        // builder.Services.AddControllers()
+        //
+        // let app = builder.Build()
+        //
+        // app.UseHttpsRedirection()
+        //
+        // app.UseAuthorization()
+        // app.MapControllers()
+        //
+        // app.Run()
+        //
+        let userActions =
+            handle
+                "users"
+                { ListUsers = UserRepository.getUsers
+                  GetUser = UserRepository.getUser
+                  AddUser = UserRepository.createUser
+                  UpdateUser = UserRepository.updateUser
+                  UpdateUserById = UserRepository.updateUserById
+                  DeleteUser = UserRepository.deleteUser }
 
-        builder.Services.AddControllers()
-
-        let app = builder.Build()
-
-        app.UseHttpsRedirection()
-
-        app.UseAuthorization()
-        app.MapControllers()
-
-        app.Run()
+        startWebServer defaultConfig userActions
 
         exitCode
--- Services/UserService.fs
+++ Services/UserService.fs
@@ -23,3 +23,45 @@ module UserService =
 
         JsonConvert.SerializeObject(v, jsonSerializerSettings) |> OK
         >=> Writers.setMimeType "application/json"
+
+    type Action<'t> =
+        { ListUsers: unit -> 't seq
+          GetUser: int -> 't option
+          AddUser: 't -> 't
+          UpdateUser: 't -> 't option
+          UpdateUserById: int -> 't -> 't option
+          DeleteUser: int -> unit }
+
+    let getActionData<'t> (req: HttpRequest) =
+        req.rawForm |> getUTF8 |> jsonToObject<'t>
+
+    let handle nameOfAction action =
+        let badRequest = BAD_REQUEST "Oops, something went wrong here !"
+        let notFound = NOT_FOUND "Oops, I couldn't find that!"
+
+        let handleAction reqError =
+            function
+            | Some r -> r |> JSON
+            | _ -> reqError
+
+        let listAll = warbler (fun _ -> action.ListUsers() |> JSON)
+        let getById = action.GetUser >> handleAction notFound
+
+        let updateById id =
+            request (getActionData >> (action.UpdateUserById id) >> handleAction badRequest)
+
+        let deleteById id =
+            action.DeleteUser id
+            NO_CONTENT
+
+        let actionPath = "/" + nameOfAction
+
+        choose
+            [ path actionPath
+              >=> choose
+                  [ GET >=> listAll
+                    POST >=> request (getActionData >> action.AddUser >> JSON)
+                    PUT >=> request (getActionData >> action.UpdateUser >> handleAction badRequest) ]
+              DELETE >=> pathScan "/users/%d" (fun id -> deleteById id)
+              GET >=> pathScan "/users/%d" (fun id -> getById id)
+              PUT >=> pathScan "/users/%d" (fun id -> updateById id) ]
